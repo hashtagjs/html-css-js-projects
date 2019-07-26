6eecfcb3e52e80f6d3aa9ecc888f629d7d14a410
hashtagjs <52577257+hashtagjs@users.noreply.github.com>
2019-07-26 17:00:31 +0530

    Create function.js

---
+++ function.js
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title> functions in js </title>
+</head>
+<body>
+hi
+<script>
+console.log("hi");
+function hw(){
+	return "hello world";
+}
+console.log(hw());
+(function(){
+	return "hello hello world";
+})();
+var a = function hws(){
+	return "hello hello hello world";
+}
+console.log(a());
+var b = function(){
+	console.log("hello hello world");
+}
+b();
+</script>
+</body>
+</html>
